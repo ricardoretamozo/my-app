354c8047d1f879254dbbc9356be6eadb254d468e
riacrdo2 <ricardo.retamozo.garay@gmail.com>
2022-05-30 02:26:51 -0500

    cambio diseño de tabla perfil

--- src/components/ui/perfil/tabla.js
+++ src/components/ui/perfil/tabla.js
@@ -1,71 +1,82 @@
 import React from 'react';
-import { Table, Thead, Tbody, Tr, Th, Td, chakra, TableContainer} from '@chakra-ui/react'
-import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
-import { useTable, useSortBy } from 'react-table'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  chakra,
+  TableContainer,
+  Box,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { useTable, useSortBy } from 'react-table';
 
 export const Tabla = () => {
+  const data = React.useMemo(
+    () => [
+      {
+        fromUnit: 'inches',
+        toUnit: 'millimetres (mm)',
+        factor: 25.4,
+      },
+      {
+        fromUnit: 'feet',
+        toUnit: 'centimetres (cm)',
+        factor: 30.48,
+      },
+      {
+        fromUnit: 'yards',
+        toUnit: 'metres (m)',
+        factor: 0.91444,
+      },
+    ],
+    []
+  );
 
-    const data = React.useMemo(
-        () => [
-          {
-            fromUnit: 'inches',
-            toUnit: 'millimetres (mm)',
-            factor: 25.4,
-          },
-          {
-            fromUnit: 'feet',
-            toUnit: 'centimetres (cm)',
-            factor: 30.48,
-          },
-          {
-            fromUnit: 'yards',
-            toUnit: 'metres (m)',
-            factor: 0.91444,
-          },
-        ],
-        [],
-      )
-    
-      const columns = React.useMemo(
-        () => [
-          {
-            Header: 'To convert',
-            accessor: 'fromUnit',
-          },
-          {
-            Header: 'Into',
-            accessor: 'toUnit',
-          },
-          {
-            Header: 'Multiply by',
-            accessor: 'factor',
-            isNumeric: true,
-          },
-        ],
-        [],
-      )
-    
-      const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-        useTable({ columns, data }, useSortBy)
-    
-      return (
-        <TableContainer>
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'To convert',
+        accessor: 'fromUnit',
+      },
+      {
+        Header: 'nuevos',
+        accessor: 'toUnit',
+      },
+      {
+        Header: 'Multiply by 2',
+        accessor: 'factor',
+        isNumeric: true,
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
+
+  return (
+    <Box  borderWidth='1px' borderRadius='lg' overflow='hidden' bg={useColorModeValue('white', 'gray.900')}>
+      <TableContainer>
         <Table {...getTableProps()}>
           <Thead>
-            {headerGroups.map((headerGroup) => (
+            {headerGroups.map(headerGroup => (
               <Tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
+                {headerGroup.headers.map(column => (
                   <Th
                     {...column.getHeaderProps(column.getSortByToggleProps())}
                     isNumeric={column.isNumeric}
                   >
                     {column.render('Header')}
-                    <chakra.span pl='4'>
+                    <chakra.span pl="4">
                       {column.isSorted ? (
                         column.isSortedDesc ? (
-                          <TriangleDownIcon aria-label='sorted descending' />
+                          <TriangleDownIcon aria-label="sorted descending" />
                         ) : (
-                          <TriangleUpIcon aria-label='sorted ascending' />
+                          <TriangleUpIcon aria-label="sorted ascending" />
                         )
                       ) : null}
                     </chakra.span>
@@ -75,20 +86,24 @@ export const Tabla = () => {
             ))}
           </Thead>
           <Tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row)
+            {rows.map(row => {
+              prepareRow(row);
               return (
                 <Tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => (
-                    <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                  {row.cells.map(cell => (
+                    <Td
+                      {...cell.getCellProps()}
+                      isNumeric={cell.column.isNumeric}
+                    >
                       {cell.render('Cell')}
                     </Td>
                   ))}
                 </Tr>
-              )
+              );
             })}
           </Tbody>
         </Table>
-        </TableContainer>
-      )
-  };
+      </TableContainer>
+    </Box>
+  );
+};
